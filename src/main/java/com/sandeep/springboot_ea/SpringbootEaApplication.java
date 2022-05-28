b0333e64baf10753628f377b6adbc564d9ae45f3
sandeeppl <sandeeppavagada@gmail.com>
2022-05-28 18:28:58 +0530

    Initial Commit

---
+++ src/main/java/com/sandeep/springboot_ea/SpringbootEaApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.springboot_ea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandeep/springboot_ea/SpringbootEaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.springboot_ea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
