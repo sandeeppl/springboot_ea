957b8f4a29b64f758ae9aeb03a4b0439d1e13e69
sandeeppl <sandeeppavagada@gmail.com>
2022-06-04 16:36:23 +0530

    Added new test file

---
+++ src/test/java/com/sandeep/springboot_ea/SpringbootEaApplicationTests2.java
@@ -0,0 +1,17 @@
+package com.sandeep.springboot_ea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEaApplicationTests2 {
+
+	@Test
+	void contextLoads() {
+
+		System.out.println("Test12");
+
+
+	}
+
+}
